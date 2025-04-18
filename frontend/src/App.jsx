337d57eb62227c5a6f3b41a04628f4790a182efc
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-04-18 17:43:23 +0530

    created a basic fontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LandingPage from "./pages/landingPage/LandingPage"
+import ItemDetailsPage from "./components/ItemDetailPage/ItemDetailsPage"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import Cart from "./components/cartPage/Cart"
+import PrivateRoute from "./privateRoute/privateRoute"
+import SearchItems from "./components/searchItem/SearchItems"
 
-const App = () => {
+const App=()=> {
   return (
-    <div>App</div>
+      <BrowserRouter basename='/'>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/products/:id" element={<ItemDetailsPage/>}/>
+        <Route path="/cart" element={<PrivateRoute>
+          <Cart/>
+        </PrivateRoute>}/>
+        <Route path="/search/:value" element={<SearchItems/>}/>
+      </Routes>
+      <ToastContainer/>
+      </BrowserRouter>
   )
 }
-
 export default App
