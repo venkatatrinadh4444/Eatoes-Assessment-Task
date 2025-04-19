1c37b159cfd171e233c8df93cd346136dc2c8155
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-04-19 18:06:56 +0530

    updated the backend url with all the api end points

--- frontend/src/components/Api/Api.jsx
+++ frontend/src/components/Api/Api.jsx
@@ -1,3 +1,3 @@
-export const API_URI="http://localhost:8000"
+/* export const API_URI="http://localhost:8000" */
 
-/* export const API_URI="https://multi-vendor-swiggy-clone.onrender.com" */
+export const API_URI="https://eatoes-assessment-task.onrender.com"
